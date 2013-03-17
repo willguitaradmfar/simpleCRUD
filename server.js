67b96e10d27521a11b55c5139ebe3e6e80187c6d
Javier Tomás Zon <jtomaszon@gmail.com>
2013-03-17 19:53:04 -0300

    Adding express to our project

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express"),
+    app = express();
+
+var redis = require("redis"),
+    client = redis.createClient();
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {
+        layout: false
+    });
+
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler()); 
+});
+
+app.listen(3000);
